d7e8b2d0687ef89b56325866ef7e99d81a1848a8
Khadija2504 <khadijalady25@gmail.com>
2025-03-03 15:29:36 +0000

    CPW-32 update the routing for the can activate role guards

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { CourseListComponent } from './course-list-component/course-list.component';
 import { AddCourseComponent } from './add-course/add-course.component';
 import { UpdateCourseComponent } from './update-course/update-course.component';
+import { RoleGuard } from '../auth/role.guard';
 
 const routes: Routes = [
-  {path: '', component: CourseListComponent},
-  {path: 'add-course', component: AddCourseComponent},
-  {path: 'update-course/:courseId', component: UpdateCourseComponent},
+  {path: '', component: CourseListComponent, canActivate: [RoleGuard], data: { expectedRole: 'EMPLOYEE' }},
+  {path: 'add-course', component: AddCourseComponent, canActivate: [RoleGuard], data: { expectedRole: 'ADMIN' }},
+  {path: 'update-course/:courseId', component: UpdateCourseComponent, canActivate: [RoleGuard], data: { expectedRole: 'ADMIN' } },
 ];
 
 @NgModule({
--- src/app/library/library-routing.module.ts
+++ src/app/library/library-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ResourcesListComponent } from './resources-list/resources-list.component';
 import { AddResourceComponent } from './add-resource/add-resource.component';
+import { RoleGuard } from '../auth/role.guard';
 
 const routes: Routes = [
-  {path: '', component: ResourcesListComponent},
-  {path: 'resource-form', component: AddResourceComponent},
+  {path: '', component: ResourcesListComponent, canActivate: [RoleGuard], data: { expectedRole: 'EMPLOYEE'}},
+  {path: 'resource-form', component: AddResourceComponent, canActivate: [RoleGuard], data: {expectedRole: 'ADMIN'}},
 ];
 
 @NgModule({

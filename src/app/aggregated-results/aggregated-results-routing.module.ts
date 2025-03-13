1124c297e6c7617ec9664c116d152e764f035431
Khadija2504 <khadijalady25@gmail.com>
2025-03-13 09:32:59 +0000

    CPW-24 correct the authorization for routing

--- src/app/aggregated-results/aggregated-results-routing.module.ts
+++ src/app/aggregated-results/aggregated-results-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AggregatedResultsListComponent } from './aggregated-results-list/aggregated-results-list.component';
 import { EmployeeRankingComponent } from './employee-ranking/employee-ranking.component';
+import { RoleGuard } from '../auth/role.guard';
 
 const routes: Routes = [
-  {path: '', component: AggregatedResultsListComponent},
-  {path: 'ranking', component: EmployeeRankingComponent}
+  {path: '', component: AggregatedResultsListComponent, canActivate: [RoleGuard], data: { expectedRole: 'ADMIN' }},
+  {path: 'ranking', component: EmployeeRankingComponent, canActivate: [RoleGuard], data: { expectedRole: 'EMPLOYEE' }}
 ];
 
 @NgModule({

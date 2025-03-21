e4acdd82ddf4799782eb649cd69a9f21fc7c1ea7
Khadija2504 <khadijalady25@gmail.com>
2025-03-21 11:18:21 +0000

    CPW-43 add the authoriz guard to check the authentication befor access to the auth pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { RoleGuard } from './auth/role.guard';
 import { AuthGuard } from './auth/auth.guard';
+import { AuthorizGuard } from './auth/authoriz.guard';
 
 const routes: Routes = [
   {path: '', redirectTo: '/auth/login', pathMatch: 'full'},
-  {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m=> m.AuthModule)},
+  {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m=> m.AuthModule), canActivate: [AuthorizGuard]},
   {path: 'error', loadChildren: () => import('./errors/errors.module').then(m=> m.ErrorsModule)},
   {path: 'profile', loadChildren: () => import('./profile/profile.module').then(m=> m.ProfileModule), canActivate: [AuthGuard]},
   {path: 'mentorShip-coaching', loadChildren: () => import('./mentor-ship/mentor-ship.module').then(m=> m.MentorShipModule), canActivate: [AuthGuard]},
